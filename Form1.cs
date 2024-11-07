7be3c64ec4c63b45629e450085d86e398d6fe558
GlebSotsjov <153904865+GlebSotsjov@users.noreply.github.com>
2024-11-07 09:57:37 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangle
+{
+    public partial class Form1 : Form
+    {
+        Button btn;
+        TextBox txtA, txtB, txtC;
+        ListView listView1;
+        Label labelA, labelB, labelC;
+
+        PictureBox pictureBox;
+        public Form1()
+        {
+            this.Height = 500;
+            this.Width = 800;
+            this.Text = "Töötamine kolmnurgaga";
+            //Button - btn
+            btn = new Button();
+            btn.Text = "Käivitamine";
+            btn.Font = new Font("Arial", 18, FontStyle.Italic);
+            btn.AutoSize = true;
+            btn.Height = 80;
+            btn.Width = 50;
+            btn.FlatAppearance.BorderSize = 6;
+            btn.FlatAppearance.BorderColor = Color.LightSeaGreen;
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.Location = new Point(450, 20);
+            btn.BackColor = Color.Khaki;
+            Controls.Add(btn);
+            btn.Click += Btn_Click;
+
+            //TextBox - txtA
+            txtA = new TextBox();
+            txtA.Location = new Point(160, 350);
+            txtA.Font = new Font("Arial", 13);
+            txtA.Width = 100;
+            txtA.Height = 80;
+            Controls.Add(txtA);
+
+            //Label - labelA
+            labelA = new Label();
+            labelA.Text = "Külg A";
+            labelA.Font = new Font("Arial", 13, FontStyle.Bold | FontStyle.Underline);
+            labelA.ForeColor = Color.RoyalBlue;
+            labelA.AutoSize = true;
+            labelA.Location = new Point(90, 350);
+            Controls.Add(labelA);
+
+            //TextBox - txtB
+            txtB = new TextBox();
+            txtB.Location = new Point(160, 380);
+            txtB.Font = new Font("Arial", 13);
+            txtB.Width = 100;
+            txtB.Height = 80;
+            Controls.Add(txtB);
+
+            //Label - labelB
+            labelB = new Label();
+            labelB.Text = "Külg B";
+            labelB.Font = new Font("Arial", 13, FontStyle.Bold | FontStyle.Underline);
+            labelB.ForeColor = Color.RoyalBlue;
+            labelB.AutoSize = true;
+            labelB.Location = new Point(90, 380);
+            Controls.Add(labelB);
+
+            //TextBox - txtC
+            txtC = new TextBox();
+            txtC.Location = new Point(160, 410);
+            txtC.Font = new Font("Arial", 13);
+            txtC.Width = 100;
+            txtC.Height = 80;
+            Controls.Add(txtC);
+
+            //Label - labelC
+            labelC = new Label();
+            labelC.Text = "Külg C";
+            labelC.Font = new Font("Arial", 13, FontStyle.Bold | FontStyle.Underline);
+            labelC.ForeColor = Color.RoyalBlue;
+            labelC.AutoSize = true;
+            labelC.Location = new Point(90, 410);
+            Controls.Add(labelC);
+
+            //listView1
+            listView1 = new ListView();
+            listView1.Location = new Point(10, 10);
+            listView1.Font = new Font("Arial", 10);
+            listView1.Width = 360;
+            listView1.Height = 250;
+            listView1.View = View.Details;
+            listView1.Columns.Add("Väli", 165);
+            listView1.Columns.Add("Väärtused", 190);
+            listView1.BackColor = Color.Silver;
+            listView1.ForeColor = Color.DarkBlue;
+            Controls.Add(listView1);
+
+            //pictureBox
+            pictureBox = new PictureBox();
+            pictureBox.Location = new Point(450, 150);
+            pictureBox.Size = new Size(300, 300);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            Controls.Add(pictureBox);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            c = Convert.ToDouble(txtC.Text);
+
+            Triangle triangle = new Triangle(a, b, c);
+
+            listView1.Items.Clear();
+            listView1.Items.Add("Külg A");
+            listView1.Items.Add("Külg B");
+            listView1.Items.Add("Külg C");
+            listView1.Items.Add("Периметр");
+            listView1.Items.Add("Piirkond");
+            listView1.Items.Add("Kõrgus küljele A");
+            listView1.Items.Add("Kõrgus küljele B");
+            listView1.Items.Add("Kõrgus küljele C");
+            listView1.Items.Add("Kas see on olemas?");
+            listView1.Items.Add("Täpsustaja");
+            listView1.Items[0].SubItems.Add(triangle.outputA());
+            listView1.Items[1].SubItems.Add(triangle.outputB());
+            listView1.Items[2].SubItems.Add(triangle.outputC());
+            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));
+            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
+            listView1.Items[5].SubItems.Add(Convert.ToString(triangle.HeightA()));
+            listView1.Items[6].SubItems.Add(Convert.ToString(triangle.HeightB()));
+            listView1.Items[7].SubItems.Add(Convert.ToString(triangle.HeightC()));
+            if (triangle.ExistTriange)
+            {
+                listView1.Items[8].SubItems.Add("On olemas");
+            }
+            else
+            {
+                listView1.Items[8].SubItems.Add("Ei ole olemas");
+            }
+            //Метод, позволяющий определить тип треугольника и определив тип,
+            //меняющий отображаемую картинку на соответствующую.
+            //Название типа треугольника отобразите в значении спецификатора.
+            if (triangle.ExistTriange)
+            {
+                if (triangle.TriangleType == "Võrdkülgne")
+                {
+                    listView1.Items[9].SubItems.Add("Võrdkülgne");
+                    pictureBox.Image = Image.FromFile(@"..\..\Vordkulgne.png");
+                }
+                else if (triangle.TriangleType == "Võrdhaarne")
+                {
+                    listView1.Items[9].SubItems.Add("Võrdhaarne");
+                    pictureBox.Image = Image.FromFile(@"..\..\Vordhaarne.png");
+                }
+                else if (triangle.TriangleType == "Skaleeni kolmnurk")
+                {
+                    listView1.Items[9].SubItems.Add("Skaleeni kolmnurk");
+                    pictureBox.Image = Image.FromFile(@"..\..\Skaleeni kolmnurk.jpg");
+                }
+                else
+                {
+                    pictureBox.Image = null;
+                }
+            }
+            else
+            {
+                pictureBox.Image = null;
+            }
+
+            pictureBox.Invalidate(); // Обновляем PictureBox, чтобы отобразить новую картинку
+
+            // Добавьте еще один метод на кнопку Запуск. Для перехода с первой формы на вторую.
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        //одно поле, помимо a,b и c, это высота
+        public double h;
+        //одно поле, помимо a,b и c, у меня это угол
+        public double nurk;
+        public Triangle() { } //один конструктор без параметров
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+        public Triangle(double A, double B, double C, double H, double Nurk)
+        {
+            a = A;
+            b = B;
+            c = C;
+            nurk = Nurk;
+            h = H;
+        }
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public string outputnurk()
+        {
+            return Convert.ToString(nurk);
+        }
+        public string outputH()
+        {
+            return Convert.ToString(h);
+        }
+        public double Perimeter()
+        {
+            double p = 0;
+            p = a + b + c;
+            return p;
+        }
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+        //один метод для нахождения полупериметра
+        public double Poolperimeetrit()
+        {
+            return (a + b + c) / 2;
+        }
+
+        //одно свойство
+        public double PindalaArvutamine()
+        {
+            return 0.5 * a * b * Math.Sin(nurk * Math.PI / 180);  // nurk переводится в радиан, чтобы значение не было отрицательным
+        }
+        // Метод для нахождения высоты к стороне a
+        public double HeightA()
+        {
+            double s = Surface();
+            return (2 * s) / a;
+        }
+
+        // Метод для нахождения высоты к стороне b
+        public double HeightB()
+        {
+            double s = Surface();
+            return (2 * s) / b;
+        }
+
+        // Метод для нахождения высоты к стороне c
+        public double HeightC()
+        {
+            double s = Surface();
+            return (2 * s) / c;
+        }
+        public double GetSetA
+        {
+            get
+            { return a; }
+            set
+            { a = value; }
+        }
+        public double GetSetB
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        public double GetSetC
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+        public bool ExistTriange
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else return true;
+            }
+        }
+        public string TriangleType
+        {
+            get
+            {
+                if (ExistTriange)
+                {
+                    if (a == b && b == c && a == c)
+                    {
+                        return "Võrdkülgne";
+                    }
+                    else if (a == b || b == c || a == c)
+                    {
+                        return "Võrdhaarne";
+                    }
+                    else
+                    {
+                        return "Skaleeni kolmnurk";
+                    }
+                }
+                else
+                {
+                    return "Tundmatu tüüp";
+                }
+            }
+        }
+        private double Leida_nurki(double x, double y, double z)
+        {
+            //нахождения арккосинуса (обратная функция косинуса)
+            return Math.Acos((y * y + z * z - x * x) / (2 * y * z)) * (180 / Math.PI);
+        }
+        public string TriangleType_Form2
+        {
+            get
+            {
+                if (ExistTriange)
+                {
+                    if (a == b && b == c)
+                    {
+                        return "Võrdkülgne";  // Равносторонний
+                    }
+                    else if (a == b || b == c || a == c)
+                    {
+                        return "Võrdhaarne";  // Равнобедренный
+                    }
+                    else
+                    {
+                        double nurk = Leida_nurki(a, b, c);
+                        double nurkB = Leida_nurki(b, a, c);
+                        double nurkC = Leida_nurki(c, a, b);
+
+                        if (nurk == 90 || nurkB == 90 || nurkC == 90)
+                        {
+                            return "Täisnurkne";  // Прямоугольный
+                        }
+                        else if (nurk < 90 && nurkB < 90 && nurkC < 90)
+                        {
+                            return "Teravnurkne";  // Остроугольный
+                        }
+                        else
+                        {
+                            return "Nürinurkne";  // Тупоугольный
+                        }
+                    }
+                }
+                else
+                {
+                    return "Tundmatu tüüp";  // Неопределенный
+                }
+            }
+        }
+        // Метод для сохранения треугольников в XML-файл
+        public static void Salvesta(List<Triangle> triangles, string filePath = @"..\..\kolmnurgad.xml")
+        {
+            try
+            {
+                // Настройка XML Writer для форматированного вывода
+                XmlWriterSettings vormingus = new XmlWriterSettings();
+
+                vormingus.Indent = true; //  отступы для удобочитаемости
+                vormingus.IndentChars = "    "; // 4 пробела для отступов
+                vormingus.NewLineOnAttributes = false; // Атрибуты оставляются на той же строке
+
+                // Создается XML Writer для записи в файл
+                using (XmlWriter xmlwriter = XmlWriter.Create(filePath, vormingus))
+                {
+                    xmlwriter.WriteStartDocument(); // Начинает
+                    xmlwriter.WriteStartElement("Triangles"); // Открывает элемент "Triangles"
+
+                    foreach (var triangle in triangles)
+                    {
+                        xmlwriter.WriteStartElement("Triangle");
+
+                        // Записываем каждый атрибут треугольника как отдельный элемент
+                        xmlwriter.WriteElementString("Külg_A", triangle.a.ToString());
+                        xmlwriter.WriteElementString("Külg_B", triangle.b.ToString());
+                        xmlwriter.WriteElementString("Külg_C", triangle.c.ToString());
+                        xmlwriter.WriteElementString("Kõrgus", triangle.h.ToString());
+                        xmlwriter.WriteElementString("Nurk", triangle.nurk.ToString());
+                        xmlwriter.WriteElementString("Poolperimeeter", triangle.Poolperimeetrit().ToString());
+                        xmlwriter.WriteElementString("Piirkond", triangle.PindalaArvutamine().ToString());
+                        xmlwriter.WriteElementString("Täpsustaja", triangle.TriangleType_Form2);
+
+                        xmlwriter.WriteEndElement(); // Закрываем элемент "Triangle" 
+                    }
+
+                    xmlwriter.WriteEndElement(); // Закрывает элемент "Triangles"
+                    xmlwriter.WriteEndDocument(); // Завершаем
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Viga faili salvestamisel: " + ex.Message, "Viga!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
